961f175333daab7258281d647fb4253c1c5f0662
Christian Wygoda <info@wygoda.net>
2015-06-07 22:00:24 +0200

    Config simplification, Celery routing

--- kinderstadt_passgen/config/defaults.py
+++ kinderstadt_passgen/config/defaults.py
@@ -1,16 +1,35 @@
 import logging
 from path import Path
+from kombu import Exchange, Queue
 
 
 _BASE_DIR = Path(__file__).abspath().dirname().dirname()
 
 SECRET_KEY = '5m34a58x(3^$np08v!si#!a1btp$(h$a0qa-j_c)^!-ah=ypqs'
 
+# Database settings
+SQLALCHEMY_DATABASE_URI = 'postgresql://kinderstadt@localhost/kinderstadt_passgen'
+
 # Celery settings
+CELERY_BROKER_URL = 'amqp://kinderstadt:kinderstadt@localhost/passgen'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERYD_CONCURRENCY = 2
+
+
+CELERY_QUEUES = (
+    Queue('passgen',
+          Exchange('passgen', type='topic'),
+          routing_key='kinderstadt.passgen'),
+)
+
+CELERY_ROUTES = ({
+    'kinderstadt_passgen.tasks.execute_order': {
+        'queue': 'passgen',
+        'routing_key': 'kinderstadt.passgen'
+    }
+},)
 
 # App specific settings
 ID_ENCODE_OFFSET = 10000
--- kinderstadt_passgen/config/development.py
+++ kinderstadt_passgen/config/development.py
@@ -1,11 +1,7 @@
 import logging
 
 
-# Database settings
-SQLALCHEMY_DATABASE_URI = 'postgresql://kinderstadt@localhost/kinderstadt_passgen'
-
 # Celery settings
-CELERY_BROKER_URL = 'amqp://guest:guest@localhost/'
 CELERYD_LOG_LEVEL = 'INFO'
 
 # App specific settings
