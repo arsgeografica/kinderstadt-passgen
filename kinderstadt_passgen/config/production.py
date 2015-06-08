3f5803e72143d3b746be8facb8f375723fbf5cb5
Christian Wygoda <info@wygoda.net>
2015-06-08 22:59:13 +0200

    More provisioning fixes, changes and fonts

--- kinderstadt_passgen/config/production.py
+++ kinderstadt_passgen/config/production.py
@@ -1,4 +1,6 @@
 from kinderstadt_passgen.config.defaults import _BASE_DIR
 
 # App specific settings
+FILE_STORAGE_PATH = _BASE_DIR / '../../media'
+
 LOG_FILE = _BASE_DIR / '../../logs/passgen.log'
